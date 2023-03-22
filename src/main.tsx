9de5abfda49e7f2b0130a42350a5ecdba8cf279a
weilderbeast <bogdan.is.a.beast@gmail.com>
2023-03-22 13:36:48 +0100

    initial test

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { Canvas } from "@react-three/fiber";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Canvas
+      gl={{ antialias: false, stencil: false }}
+      camera={{ position: [2, 0, 0], fov: 60 }}
+    >
+      <App />
+    </Canvas>
+  </React.StrictMode>
+);
