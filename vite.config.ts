5e3ef77fce3eb90cff34c433bc593e11aa267526
weilderbeast <bogdan.is.a.beast@gmail.com>
2023-05-31 11:36:14 +0200

    don't define globalThis in production

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,24 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 8080,
-  },
-  envPrefix: "REACT_APP_",
-  build: {
-    sourcemap: true,
-  },
+export default defineConfig(({ mode }) => {
+  const config = {
+    plugins: [react()],
+    server: {
+      port: 8080,
+    },
+    envPrefix: "REACT_APP_",
+    build: {
+      sourcemap: true,
+    },
+    define: {},
+  };
+
+  if (mode === "development") {
+    config.define = {
+      global: "globalThis",
+    };
+  }
+
+  return config;
 });
